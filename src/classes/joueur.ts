f379c95f12bcd3aedc730fb059f6733948d88887
Massinissa <massi.djerrah59@gmail.com>
2020-03-27 17:40:57 +0100

    Player class coding

--- src/classes/joueur.ts
+++ src/classes/joueur.ts
@@ -1,1 +1,29 @@
 //TODO
+import { Gobelet } from './gobelet';
+import { de } from './de';
+
+
+export class Joueur {
+
+    private _nom: string;
+    private _score: number;
+
+    constructor(name: string) {
+        this._nom = name;
+        this._score = 0;
+    }
+
+    public get name(): string {
+        return this._nom;
+    }
+
+    public get score(): number {
+        return this._score;
+    }
+
+    public jouer = (gobelet: Gobelet) => {
+        gobelet.lancer(); 
+        this._score += gobelet.getValeur(); 
+    }
+
+}
