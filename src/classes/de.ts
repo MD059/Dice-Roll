7b6aba52689872840dc3de2a306946eb426b3fb7
Massinissa <massi.djerrah59@gmail.com>
2020-03-27 17:41:59 +0100

    Dice class coding

--- src/classes/de.ts
+++ src/classes/de.ts
@@ -1,1 +1,21 @@
 //TODO
+
+export class de {
+
+    private _valeur: number;
+    //------
+    private Min: number = 1; 
+    private Max: number = 6; 
+
+    constructor() {
+        this._valeur = 0;
+    }
+
+    public get value(): number {
+        return this._valeur;
+    }
+
+    public lancer = () => {
+        return this.Min + Math.floor((Math.random() * (this.Max - this.Min))); 
+    }
+}
